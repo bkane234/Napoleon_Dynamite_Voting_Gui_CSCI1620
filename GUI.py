72fc5111ed2fe0c760a21be1f977ea70fb7ae4e3
bkane234 <bkane@unomaha.edu>
2024-12-13 11:31:34 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+#This GUI file creates each of the various widgets and places them accordingly on the window
+
 from tkinter import * 
 
 
@@ -20,25 +22,25 @@ class GUI:
         self.label_voting_pane = Label(self.window, text="Choose one candidate:")
         
 
-        pedro_image = PhotoImage(file=r"C:\Users\bkane\OneDrive\Documents\GitHub\Voting_Lab_Napoleon_Dynamite\Pedro.gif")
-        pedro_image = pedro_image.subsample(4,4)
+        pedro_image = PhotoImage(file=r"C:\Users\bkane\OneDrive\Documents\GitHub\Voting_Lab_Napoleon_Dynamite\Pedro.gif")#Creates the images for use with radio buttons
+        pedro_image = pedro_image.subsample(4,4)#Resizes said photos
         summer_image = PhotoImage(file=r"C:\Users\bkane\OneDrive\Documents\GitHub\Voting_Lab_Napoleon_Dynamite\Summer.gif")
         summer_image = summer_image.subsample(4,4)
         #Creates the images for use with radio buttons
 
-        self.entry_voting_name = Entry(self.window, width=20)
+        self.entry_voting_name = Entry(self.window, width=20)#Creates various input boxes
         self.entry_voting_ID = Entry(self.window, width=20)
         self.entry_voting_DOB = Entry(self.window, width=15)
         self.option.set(None)
         self.radio_pedro = Radiobutton(self.window, text= "Pedro Sanchez",variable=self.option, value="Pedro_Sanchez", image=pedro_image, compound="left")
-        self.radio_pedro.image = pedro_image 
+        self.radio_pedro.image = pedro_image # ---> Courtesy of Fandom.com
         self.radio_summer = Radiobutton(self.window, text= "Summer Wheatly",variable=self.option, value="Summer_Wheatly", image=summer_image, compound ="left")
-        self.radio_summer.image = summer_image
+        self.radio_summer.image = summer_image # ---> Courtesy of Tmz.com
         self.button_submit = Button(self.window, text="Submit", command=self.submit)
         self.button_view_results = Button(self.window, text="View Results", command=self.view_results)
-        #Creates various input boxes
+        
 
-        self.label_voting_name.pack(side="top", padx = 10, pady=15, anchor="w")
+        self.label_voting_name.pack(side="top", padx = 10, pady=15, anchor="w") #Places each widget
         self.entry_voting_name.place(x=150, y=17)
         self.label_voting_ID.pack(side="top", padx = 10, pady=15, anchor="w")
         self.entry_voting_ID.place(x=150, y=67)
@@ -49,15 +51,15 @@ class GUI:
         self.radio_summer.pack(side="top", pady=10, anchor="w")
         self.button_view_results.pack(side="bottom", padx =10, anchor="center")
         self.button_submit.pack(side="bottom", padx =10, anchor="center")
-        #Places each widget
-    def submit(self):
+       
+    def submit(self): #These functions help to import and call functions from the logic file. 
         from Logic import submit  
         submit(self)
     def view_results(self):
         from Logic import view_results  
         view_results(self)  
     
-class Results(GUI):
+class Results(GUI): #Results is a child class of GUI that places the widgets on the Results window
     def __init__(self, window1, total_pedro, total_summer) -> None:
     
         self.window = window1
